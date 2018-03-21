f17a37c44130edfc41851d5d3bd6b9879c030aef
lvbernal <lvbernal@gmail.com>
2018-03-21 16:47:59 -0500

    mockup ui elements added

--- SpectrumFees/ViewModels/MainViewModel.cs
+++ SpectrumFees/ViewModels/MainViewModel.cs
@@ -6,47 +6,8 @@ namespace SpectrumFees.ViewModels
 {
     public class MainViewModel : BindableBase
     {
-        private SimpleCalculationViewModel _simpleCalculation;
-        public SimpleCalculationViewModel SimpleCalculation
-        {
-            get => _simpleCalculation;
-            set
-            {
-                _simpleCalculation = value;
-                NotifyPropertyChanged();
-            }
-        }
-
-        private bool _busy;
-        public bool Busy
-        {
-            get => _busy;
-            set
-            {
-                _busy = value;
-                NotifyPropertyChanged();
-                ShowSimpleCalculationDescriptionCommand.ChangeCanExecute();
-            }
-        }
-
         public MainViewModel()
         {
-            ShowSimpleCalculationDescriptionCommand = new Command(ShowSimpleCalculationDescription, () => !Busy);
-        }
-
-        public Command ShowSimpleCalculationDescriptionCommand { get; }
-        private async void ShowSimpleCalculationDescription()
-        {
-            Busy = true;
-            FreeResources();
-            SimpleCalculation = new SimpleCalculationViewModel();
-            await Application.Current.MainPage.Navigation.PushAsync(new SimpleCalculationPage());
-            Busy = false;
-        }
-
-        private void FreeResources()
-        {
-            SimpleCalculation = null;
         }
     }
 }
