454b3fb6d62be558d69238dd68808f78551ef207
lvbernal <lvbernal@gmail.com>
2018-03-23 11:33:57 -0500

    ParametersVisibilityHelper added

---
+++ SpectrumFees/Helpers/ParametersVisibilityHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace SpectrumFees.Helpers
+{
+    public class ParametersVisibilityHelper
+    {
+        private static string[] mobileParameters = new string[] { "Occupancy", "CellFactor" };
+        private static string[] mvdsParameters = new string[] { "Occupancy", "PowerFactor", "SiteFactor" };
+        private static string[] broadcastParameters = new string[] { "Occupancy", "PowerFactor", "WeightFactor" };
+        private static string[] mwp2pParameters = new string[] { "Occupancy", "ServiceFactor", "DirectionFactor" };
+        private static string[] pmrParameters = new string[] { };
+
+        private static string[] GetCompareArray(string service)
+        {
+            switch (service)
+            {
+                case Constants.Mobile:
+                    return mobileParameters;
+                case Constants.MVDS:
+                    return mvdsParameters;
+                case Constants.Broadcast:
+                    return broadcastParameters;
+                case Constants.MWP2P:
+                    return mwp2pParameters;
+                case Constants.PMR:
+                    return pmrParameters;
+                default:
+                    return new string[] { };
+            }
+        }
+
+        public static bool ShouldBeVisible(string service, string parameter)
+        {
+            return GetCompareArray(service).Contains(parameter);
+        }
+    }
+}
